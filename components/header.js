b7e019c105b2e7b19c86352e05a8aad4b8209319
Rodrigo Barona <rbarona@gmail.com>
2021-12-15 02:31:50 +0000

    add menus e translations

--- components/header.js
+++ components/header.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import i18n from "../lib/i18n";
 
 export default function Header() {
+  const { locale } = useRouter();
+  const formattedLocale = locale.split("-")[0];
   return (
     <>
       <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -11,9 +15,19 @@ export default function Header() {
           .
         </h2>
         <h4 className="text-center md:text-left text-lg  md:pl-8">
-          <Link href="https://nextjs.org/">
-            <a className="underline hover:text-success duration-200 transition-colors">
-              NextJS
+          <Link href="/">
+            <a className="hover:underline">{i18n.menu.home[formattedLocale]}</a>
+          </Link>{" "}
+          |{" "}
+          <Link href={i18n.menu.about.slug[formattedLocale]}>
+            <a className="hover:underline">
+              {i18n.menu.about.label[formattedLocale]}
+            </a>
+          </Link>{" "}
+          |{" "}
+          <Link href={i18n.menu.contact.slug[formattedLocale]}>
+            <a className="hover:underline">
+              {i18n.menu.contact.label[formattedLocale]}
             </a>
           </Link>
         </h4>
--- components/intro.js
+++ components/intro.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import i18n from "../lib/i18n";
+import Link from "next/link";
 
 export default function Intro() {
   const { locale } = useRouter();
@@ -10,21 +11,17 @@ export default function Intro() {
         DatoPress.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        {i18n.intro.example[formattedLocale]}{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        {i18n.intro.and[formattedLocale]}{" "}
-        <a
-          href="https://www.datocms.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          DatoCMS
-        </a>
-        .
+        <Link href={i18n.menu.about.slug[formattedLocale]}>
+          <a className="hover:underline">
+            {i18n.menu.about.label[formattedLocale]}
+          </a>
+        </Link>{" "}
+        |{" "}
+        <Link href={i18n.menu.contact.slug[formattedLocale]}>
+          <a className="hover:underline">
+            {i18n.menu.contact.label[formattedLocale]}
+          </a>
+        </Link>
       </h4>
     </section>
   );
