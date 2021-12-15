d25c5a5cb08b43a8dcb34316187594cad4dbc3b4
Rodrigo Barona <rbarona@gmail.com>
2021-12-15 04:14:48 +0000

    Update date.js

--- components/date.js
+++ components/date.js
@@ -1,6 +1,18 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
+const { es, pt, it } = require("date-fns/locale");
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  const date = parseISO(dateString);
+  console.log(date);
+  return (
+    <time dateTime={dateString}>
+      {format(date, "LLLL	d, yyyy")}
+
+      {format(date, "d 'de' LLLL 'de' yyyy", { locale: es })}
+
+      {format(date, "d 'de' LLLL 'de' yyyy", { locale: pt })}
+
+      {format(date, "d LLLL yyyy", { locale: it })}
+    </time>
+  );
 }
